58bb360bfe2681c09c6e9144f93cf445851de3cf
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-15 16:41:54 +0100

    Add setup.

--- custom_components/hellohacs/__init__.py
+++ custom_components/hellohacs/__init__.py
@@ -5,3 +5,16 @@ async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Hello Hacs component."""
     # @TODO: Add setup code.
     return True
+
+async def async_setup_entry(hass, entry):
+    """Set up hellohacs from a config entry."""
+    # Perform any setup tasks here
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Example: Set up a platform
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+
+    return True
