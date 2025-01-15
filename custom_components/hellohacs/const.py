31f3830ea4d5c32a8e2d6e06b798d70a14b398e8
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-15 16:30:57 +0100

    First cookiecutter-based version.

--- custom_components/hellohacs/const.py
+++ custom_components/hellohacs/const.py
@@ -1,8 +1,1 @@
-"""Constants for integration_blueprint."""
-
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
 DOMAIN = "hellohacs"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
