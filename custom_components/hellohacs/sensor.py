86f25f0f616225356f69a984e2349bf3527d21ca
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-15 17:27:44 +0100

    Adds logging.

--- custom_components/hellohacs/sensor.py
+++ custom_components/hellohacs/sensor.py
@@ -1,9 +1,13 @@
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import EVENT_STATE_CHANGED
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the sensor platform."""
-    async_add_entities([ExampleSensor(hass, "sensor.other_sensor")])
+    other_sensor_id = entry.data.get("other_sensor_id")
+    async_add_entities([ExampleSensor(hass, other_sensor_id)])
 
 class ExampleSensor(Entity):
     """Representation of a Sensor."""
@@ -13,6 +17,7 @@ class ExampleSensor(Entity):
         self._hass = hass
         self._other_sensor_id = other_sensor_id
         self._hass.bus.async_listen(EVENT_STATE_CHANGED, self._handle_event)
+        _LOGGER.debug("ExampleSensor initialized to listen for %s", other_sensor_id)
 
     @property
     def name(self):
@@ -26,7 +31,8 @@ class ExampleSensor(Entity):
         """Handle state changes of the other sensor."""
         if event.data.get("entity_id") == self._other_sensor_id:
             new_state = event.data.get("new_state")
+            _LOGGER.debug("Event received for %s: %s", self._other_sensor_id, new_state)
             if new_state:
                 self._state += 1
                 self.async_write_ha_state()
-
+                _LOGGER.debug("ExampleSensor state incremented to %s", self._state)
