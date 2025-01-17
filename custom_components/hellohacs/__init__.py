981212d0845ad3f8d7bdb6cfdf5079ef0c31d96e
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-17 18:03:11 +0100

    Adds an input number.

--- custom_components/hellohacs/__init__.py
+++ custom_components/hellohacs/__init__.py
@@ -7,6 +7,26 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
+    # Create the input_number entity
+    input_number_config = {
+        "name": "Elhandel Balansavgift",
+        "min": 0,
+        "max": 100,
+        "step": 0.001,
+        "unit_of_measurement": "öre/kWh",
+        "mode": "box"
+    }
+    input_number_entity_id = "input_number.supplier_balance_fee"
+    entity_registry = er.async_get(hass)
+    entity_registry.async_get_or_create(
+        domain="input_number",
+        platform="hellohacs",
+        unique_id=input_number_entity_id,
+        config_entry=entry,
+        **input_number_config
+    )
+
+
     # Await the async_forward_entry_setup call
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
