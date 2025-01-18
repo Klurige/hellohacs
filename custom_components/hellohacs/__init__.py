92993a30e5c1fa10d40cc5bf261f17b6c355bd28
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-18 09:02:56 +0100

    init

--- custom_components/hellohacs/__init__.py
+++ custom_components/hellohacs/__init__.py
@@ -1,30 +1,24 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.components.number import NumberEntity
-from homeassistant.helpers.entity import Entity
-from .const import DOMAIN
 from homeassistant.const import Platform
+from .const import DOMAIN
 
 PLATFORMS: list[Platform] = [
     Platform.NUMBER,
     Platform.SENSOR
 ]
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up hellohacs from a config entry."""
+    """Set up from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # Await the async_forward_entry_setup call
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    # Perform any cleanup tasks here
     if entry.entry_id in hass.data[DOMAIN]:
         hass.data[DOMAIN].pop(entry.entry_id)
 
