79bc0439ba25981c715f422310be161cfca81fec
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-18 06:35:05 +0100

    New take on input number.

--- custom_components/hellohacs/__init__.py
+++ custom_components/hellohacs/__init__.py
@@ -1,18 +1,51 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.components.number import NumberEntity
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_component import EntityComponent
 from .const import DOMAIN
-from homeassistant.const import Platform
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+class ExampleNumberEntity(NumberEntity):
+    def __init__(self, hass, name, initial, min_value, max_value, step):
+        self.hass = hass
+        self._name = name
+        self._state = initial
+        self._min_value = min_value
+        self._max_value = max_value
+        self._step = step
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def min_value(self):
+        return self._min_value
+
+    @property
+    def max_value(self):
+        return self._max_value
+
+    @property
+    def step(self):
+        return self._step
+
+    async def async_set_value(self, value):
+        self._state = value
+        self.async_write_ha_state()
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> bool:
     """Set up hellohacs from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-
-    # Create the input_number entity
-    input_number = InputNumber(
+    # Create the number entity
+    number_entity = ExampleNumberEntity(
         hass,
         name="Example Slider",
         initial=30,
@@ -22,15 +55,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     # Register the entity
-    component = hass.data.get("entity_components", {}).get("input_number")
-    if component is None:
-        component = EntityComponent(hass, "input_number", hass.data[DOMAIN])
-        hass.data["entity_components"]["input_number"] = component
-
-    await component.async_add_entities([input_number])
+    async_add_entities([number_entity])
 
     # Await the async_forward_entry_setup call
-    await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR, Platform.NUMBER])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
 
@@ -41,5 +69,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-
-
