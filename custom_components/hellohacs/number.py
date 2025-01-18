fb9e5bc39b197ddec0965d8f933df207849aac4e
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-18 08:39:17 +0100

    DOMAIN

--- custom_components/hellohacs/number.py
+++ custom_components/hellohacs/number.py
@@ -13,6 +13,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
