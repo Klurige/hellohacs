3b17a8631dcbd2a668f386c7182cb718297a31da
Robert Niemi <robert.niemi@cpacsystems.se>
2025-01-18 07:12:39 +0100

    numbers.

--- custom_components/hellohacs/__init__.py
+++ custom_components/hellohacs/__init__.py
@@ -11,56 +11,11 @@ PLATFORMS: list[Platform] = [
     Platform.SENSOR
 ]
 
-class ExampleNumberEntity(NumberEntity):
-    def __init__(self, hass, name, initial, min_value, max_value, step):
-        self.hass = hass
-        self._name = name
-        self._state = initial
-        self._min_value = min_value
-        self._max_value = max_value
-        self._step = step
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def min_value(self):
-        return self._min_value
-
-    @property
-    def max_value(self):
-        return self._max_value
-
-    @property
-    def step(self):
-        return self._step
-
-    async def async_set_value(self, value):
-        self._state = value
-        self.async_write_ha_state()
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> bool:
     """Set up hellohacs from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-
-    # Create the number entity
-    number_entity = ExampleNumberEntity(
-        hass,
-        name="Example Slider",
-        initial=30,
-        min_value=0,
-        max_value=100,
-        step=1
-    )
-
-    # Register the entity
-    async_add_entities([number_entity])
 
     # Await the async_forward_entry_setup call
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
